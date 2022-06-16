3c1046b66ddae3859f18d3c4a7c7b749a37ac8c3
Nhi Ly <nh915648@dal.ca>
2022-06-16 10:00:48 -0300

    Initial Commit for the home page

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import RegisterForm from './components/regform';
+import Component from './components/component'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RegisterForm/>}/>
+        <Route path="/home" element={<Component/>}/>
+        <Route path="/register/:myParam" element={<RegisterForm/>}/>
+      </Routes>
+    </BrowserRouter>
+  /*
     <div className="App">
       <Header/>
       <RegisterForm/>
+      <Component property="string"/>
     </div>
+  */
   );
 }
 
