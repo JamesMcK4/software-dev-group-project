25a7c6dfd80277a1a917ef9c569680f5c12ed3f6
your_username <ng839815@dal.ca>
2022-06-22 16:22:34 -0300

    board-drag-and-drop/Jason

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,3 +10,4 @@ ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElemen
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+git 
