45b025e222ddb397c5cc2b16d244e67f887151c8
Haoming Chen <hm778892@dal.ca>
2022-07-20 19:41:46 -0300

    create task without addTask work now

--- src/main/client/src/pages/createTask.js
+++ src/main/client/src/pages/createTask.js
@@ -1,13 +1,11 @@
-
+import CreateTaskForm from "../components/task/createtaskform";
 import {useNavigate, useParams} from "react-router-dom";
 import React, {useRef} from "react";
-import CreateTaskForm from "../components/task/createtaskform";
+
 
 const CreateTask = () => {
     const navigate=useNavigate();
-    const boardId=useParams().boardId;
-
-    function createTask(boardId,task){
+    function createTask(task){
         fetch("http://localhost:9001/task/create_task/", {
             method: 'POST',
             mode: 'cors',
@@ -15,11 +13,22 @@ const CreateTask = () => {
             headers: {'Content-Type': 'application/json'}
         }).then((res) => res.json()).
         then((data) => {
-            navigate('/board/' + boardId)
+            navigate('/board/' + data.id)
             // After you create the task, it will return a task model, which contains the task Id.
             // You need to call the backend method addTask(boardId, taskId) and pass to it the task Id of the one you just create.
             // After it returns successfully, redirect the user back to the board.
         })
+    }
+
+    async function addTask(boardId, taskId) {
+        // You can await here
+        const response = await fetch("http://localhost:9001/board/addTask/" + boardId + "/" + taskId, {
+            method: 'PUT',
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json'}
+        });
+        var data = await response.json();
+        return data;
     }
 
     return (
